c3ec26eaec69927a467fbacec793e60542cbce74
Francis Lewis <francis.lewis@me.com>
2019-04-15 16:45:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |letter|
-    puts letter
-    if letter == " "
-      puts "space !"
-    end
     if letter != " "
       counter = counter + 1
     end
   end
+  counter
 end
